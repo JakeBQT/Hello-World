78e2406660301c005a61df77a0419dd6a5249704
Jacob Babichaev <galidar333@gmail.com>
2024-10-07 19:14:23 -0400

    Second commit

--- gitdemo/src/gitDemo/HelloWorld.java
+++ gitdemo/src/gitDemo/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Some changes have been made after first commit.");
 
 	}
 
